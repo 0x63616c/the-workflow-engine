0a9b827c9bb11dc57934db39116444268d623ad5
Calum <6991398+0x63616c@users.noreply.github.com>
2026-04-12 16:29:19 -0700

    chore: remove renamed migration file (0001 -> 0002)

--- apps/api/src/db/migrations/0001_lethal_mother_askani.sql
+++
@@ -1,7 +0,0 @@
-CREATE TABLE "app_config" (
-	"id" serial PRIMARY KEY NOT NULL,
-	"key" text NOT NULL,
-	"value" jsonb NOT NULL,
-	"updated_at" timestamp DEFAULT now() NOT NULL,
-	CONSTRAINT "app_config_key_unique" UNIQUE("key")
-);
